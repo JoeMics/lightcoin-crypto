e5cbae0b923e19c647085e3a07726bb1c87e6a2a
Joseph Micla <josephmicla@gmail.com>
2021-11-06 00:12:53 +0000

    isAllowed is added. A method that returns false if you try to withdraw more than you can

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class Account {
 
   get balance() {
     // calculate balance using the transaction objects
+    if (!this.transactions[0]) return 0;
+
 
     // can't use .reduce on an array with one element
     if (this.transactions.length === 1) {
@@ -30,10 +32,14 @@ class Transaction {
   }
 
   commit() {
-    // Keep track of the time of the transaction
-    this.time = new Date();
-    // Add transaction to account
-    this.account.addTranscation(this);
+    if (this.isAllowed()) {
+      // Keep track of the time of the transaction
+      this.time = new Date();
+      // Add transaction to account
+      this.account.addTranscation(this);
+      return true;
+    }
+    return false;
   }
 }
 
@@ -41,11 +47,28 @@ class Withdrawal extends Transaction {
   get value() {
     return -this.amount;
   }
+
+  // This was MY version of isAllowed()
+  // isAllowed() {
+  //   if (this.account.balance + this.value < 0) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+
+  // Here is the less verbose, refactored isAllowed()
+  isAllowed() {
+    return (this.account.balance + this.value >= 0);
+  }
 }
 
 class Deposit extends Transaction {
   get value() {
     return this.amount;
+  }
+
+  isAllowed() {
+    return true;
   }
 }
 
@@ -53,7 +76,7 @@ class Deposit extends Transaction {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account("snow-patrol");
+const myAccount = new Account("Peter Parker");
 
 const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
@@ -62,5 +85,9 @@ console.log("This is the balance after the withdrawing $50.25", myAccount.balanc
 const t2 = new Deposit(120, myAccount);
 t2.commit();
 console.log("This is the balance after depositing $120", myAccount.balance);
+
+const t3 = new Withdrawal(120, myAccount);
+t3.commit();
+console.log("this is the balance after withdrawing ALL $120!!", myAccount.balance);
 
 console.log("These are the transactions made so far: ", myAccount.transactions);
